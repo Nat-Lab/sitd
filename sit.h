40486336e8de2ba481e03f250b6b16c3c04d46d0
Honghao Zeng <nat@nat.moe>
2020-05-03 01:25:52 -0400

    get routing ready.

--- sit.h
+++ sit.h
@@ -1,6 +1,14 @@
 #ifndef SITCTL_SIT_H
 #define SITCTL_SIT_H
 #include <stdint.h>
+#include <netlink/route/link.h>
+
+typedef struct sit_route {
+    const char *prefix;
+    uint8_t prefix_length;
+    const char *nexthop;
+    struct sit_route *next;
+} sit_route_t;
 
 typedef struct sit_config {
     const char *name;
@@ -9,6 +17,7 @@ typedef struct sit_config {
     const char *address;
     uint8_t address_mask;
     uint32_t mtu;
+    sit_route_t *routes;
 } sit_config_t;
 
 int sit_get(struct nl_sock *sk, const char *name, struct rtnl_link **link);
