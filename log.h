e410f024aeb3d6f4cb248ca060f41e8fa48b7765
Honghao Zeng <nat@nat.moe>
2020-05-03 00:54:18 -0400

    log: rename

--- log.h
+++ log.h
@@ -1,5 +1,5 @@
-#ifndef SITD_LOG
-#define SITD_LOG
+#ifndef SITCTL_LOG
+#define SITCTL_LOG
 #include <stdio.h>
 #define __log(log_level, fmt, ...) fprintf(stderr, "[" log_level "] %s: " fmt, __PRETTY_FUNCTION__, ## __VA_ARGS__)
 #define log_info(fmt, ...) __log("INFO ", fmt, ## __VA_ARGS__)
@@ -8,4 +8,4 @@
 #define log_fatal(fmt, ...) __log("FATAL", fmt, ## __VA_ARGS__)
 #define log_debug(fmt, ...) __log("DEBUG", fmt, ## __VA_ARGS__)
 
-#endif // SITD_LOG
+#endif // SITCTL_LOG
