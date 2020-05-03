e7504ac2cd7aebdd00808d6dfd5b18e7f5a323f5
Honghao Zeng <nat@nat.moe>
2020-05-03 02:34:28 -0400

    sit: libnl parse can handle prefix len

--- sit.h
+++ sit.h
@@ -5,7 +5,6 @@
 
 typedef struct sit_route {
     const char *prefix;
-    uint8_t prefix_length;
     const char *nexthop;
     struct sit_route *next;
 } sit_route_t;
@@ -15,7 +14,6 @@ typedef struct sit_config {
     const char *local;
     const char *remote;
     const char *address;
-    uint8_t address_mask;
     uint32_t mtu;
     sit_route_t *routes;
 } sit_config_t;
